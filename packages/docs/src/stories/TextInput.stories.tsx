bacb047668ce69d037bb0d0f80e9bf1a2f4d9033
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-06-15 20:31:59 -0300

    Componentes do Design System - Componente: TextArea

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -12,7 +12,7 @@ export default {
           as="label"
           css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
         >
-          <Text size={'sm'}>Email Address</Text>
+          <Text size="sm">Email Address</Text>
           {Story()}
         </Box>
       )
